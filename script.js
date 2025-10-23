10fbbc00aa95b628283ddbbbc3b4d73378218174
Matheus359660 <goncalves.santos.matheus1601@escola.pr.gov.br>
2025-10-23 14:54:29 +0000

    em teste

---
+++ script.js
@@ -0,0 +1,9 @@
+function Acender(){
+    document.getElementById("Lampada").src="on.png"
+    console.log("On")
+}
+
+function Apagar(){
+    document.getElementById("Lampada").src="off.png"
+    console.log("off")
+}
