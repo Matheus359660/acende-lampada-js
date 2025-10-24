41280d71697587d779f37ff755ba3c92529412dc
Matheus359660 <goncalves.santos.matheus1601@escola.pr.gov.br>
2025-10-24 14:54:56 +0000

    luz terminada

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function Acender(){
-    document.getElementById("Lampada").src="on.png"
-    console.log("On")
+const lampada = document.getElementById("lampada")
+
+lampada.addEventListener("mouseover",acender);
+lampada.addEventListener("mouseout",apagar);
+
+function acender(){
+    lampada.scr = "on.png";
+    document.body.style.background = "#ffeb3b22";
 }
 
-function Apagar(){
-    document.getElementById("Lampada").src="off.png"
-    console.log("off")
+function apagar(){
+    lampada.scr = "off.png";
+    document.body.style.background = "#222";
 }
