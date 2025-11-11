b08d6eb927f62aaa8c679d5221cd9dc4024cc431
Matheus359660 <goncalves.santos.matheus1601@escola.pr.gov.br>
2025-11-11 13:56:14 +0000

    ok

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const lampada = document.getElementById("lampada")
+const lampada = document.getElementById("lampada");
 
 lampada.addEventListener("mouseover",acender);
 lampada.addEventListener("mouseout",apagar);
 
 function acender(){
-    lampada.scr = "on.png";
-    document.body.style.background = "#ffeb3b22";
+    lampada.src = "on.png";
+    document.body.style.background = "#ffffffff"
 }
-
 function apagar(){
-    lampada.scr = "off.png";
-    document.body.style.background = "#222";
+    lampada.src = "off.png";
+    document.body.style.background = "#222"
+   
 }
